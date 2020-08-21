4ea20c8d172c67260720aca4c1ba9009dc4b8db5
pvineet44 <pvineet44@gmail.com>
2020-08-21 17:34:20 +0200

    back-end

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,17 @@
+class MessagesController < ApplicationController
+
+    def create
+        message = Message.new(message_params)
+        if message.save
+            render json: message
+        else
+            render json: {errors: message.errors.full_messages}, status: 422
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:content, :chat_room_id)
+    end
+end
